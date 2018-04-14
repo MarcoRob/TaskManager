a257465564825bdc4a5179abfc51b861e87f640a
MarcoRob <marcko.rp@gmail.com>
2018-04-14 17:36:00 -0500

    Setting up vuejs with materialize & basic interfaces

---
+++ src/components/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <Menu option="0"/>
+    <div class="parallax-container center valign-wrapper">
+      <div class="container">
+        <h2 class="left green-text darken-2"><b>The most solid <br/> tasks system manager</b></h2>
+      </div>
+      <div class="parallax"><img src="/static/people-tasks.png"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Menu from "./Menu";
+
+export default {
+  name: 'Home',
+  components : {Menu},
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted() {
+    var elem = document.querySelector('.parallax');
+    console.log(elem);
+    var instance = M.Parallax.init(elem);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.parallax-container {
+    height: 700px;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/SignIn.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+        <Menu option="1" />
+        <center>
+            <div class="section" />
+            <h3 class="purple-text">Sign In</h3>
+            <div class="section" />
+            <div class="container">
+                <div class="z-depth-1 grey lighten-4 row" style="display: inline-block; padding: 32px 48px 0px 48px; border: 1px solid #EEE;">
+                <div class="col s12">
+                    <div class='row'>
+                    <div class='col s12'>
+                    </div>
+                    </div>
+
+                    <div class='row'>
+                    <div class='input-field col s12'>
+                        <input class='validate' type='email' name='email' id='email' v-model="email"/>
+                        <label for='email'>Enter your email</label>
+                    </div>
+                    </div>
+
+                    <div class='row'>
+                    <div class='input-field col s12'>
+                        <input class='validate' type='password' name='password' id='password' v-model="password"/>
+                        <label for='password'>Enter your password</label>
+                    </div>
+                    <label style='float: right;'>
+                        <a class='pink-text' href='#!'><b>Forgot Password?</b></a>
+                    </label>
+                    </div>
+
+                    <br />
+                    <center>
+                    <div class='row'>
+                        <button type='submit' @click="login" name='btn_login' class='col s12 btn btn-large waves-effect light-green darken-1'>
+                            <b>Login</b>
+                        </button>
+                    </div>
+                    </center>
+                </div>
+                </div>
+            </div>
+            <router-link to="/signup">Create account</router-link>
+        </center>
+    </div>
+
+
+</template>
+
+<script>
+import Menu from "./Menu";
+
+export default {
+    data() {
+        return {
+            email: "",
+            password: ""
+        }
+    }, 
+    components: { Menu },
+    methods: {
+        login() {
+            alert(this.email + " " + this.password);
+        }
+    }
+}
+
+
+</script>
